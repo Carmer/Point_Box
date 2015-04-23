aff5dd8de2bb7f714f582083a609cb2f97b09f32
Carmer <carmerandrew@gmail.com>
2015-04-23 13:52:08 -0600

    added user, reward and user_reward models with relationships

---
+++ app/models/reward.rb
@@ -0,0 +1,6 @@
+class Reward < ActiveRecord::Base
+
+  has_many :user_rewards
+  has_many :users,  through: :user_rewards
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :user_rewards
+  has_many :rewards, through: :user_rewards
+
 end
