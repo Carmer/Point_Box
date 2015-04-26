032f88ec9e444dfeff84399ba25cef6c485a6b29
Carmer <carmerandrew@gmail.com>
2015-04-26 14:24:35 -0600

    added styling and navbar.

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,8 +1,8 @@
 class Reward < ActiveRecord::Base
 
   has_attached_file :image, :styles => {:small => "200x200", :thumb => "100x100"}
-  validates_attachment :profile_picture, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
-  
+  validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+
   has_many :user_rewards
   has_many :users, through: :user_rewards
 
